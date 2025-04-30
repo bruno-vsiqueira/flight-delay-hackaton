09ea78cafa6832c2cdcb58857790942575721c4e
bruno-vsiqueira <bruno.siqueira@saltpay.co>
2025-04-30 14:04:18 +0100

    Refactor AirportsCubit and PredictDelayCubit to use states for better error handling and loading states

--- app/mobile/app/lib/cubit/airports_cubit.dart
+++ app/mobile/app/lib/cubit/airports_cubit.dart
@@ -3,13 +3,46 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../models/airport_model.dart';
 import '../repository/flights_repository.dart';
 
-class AirportsCubit extends Cubit<List<AirportModel>> {
+/// Cubit for fetching airports
+class AirportsCubit extends Cubit<AirportsState> {
   final FlightsRepository _repository;
 
-  AirportsCubit(this._repository) : super([]);
+  AirportsCubit(this._repository) : super(const AirportsInitial());
 
-  void fetchAirports() {
-    final airports = _repository.getAirports();
-    emit(airports);
+  Future<void> fetchAirports() async {
+    emit(const AirportsLoading());
+
+    final result = await _repository.getAirports();
+
+    result.fold(
+      (failure) =>
+          emit(AirportsError(error: failure.message ?? 'An error occurred')),
+      (airports) => emit(AirportsSuccess(airports: airports)),
+    );
   }
+}
+
+/// States for AirportsCubit
+sealed class AirportsState {
+  const AirportsState();
+}
+
+class AirportsInitial extends AirportsState {
+  const AirportsInitial();
+}
+
+class AirportsLoading extends AirportsState {
+  const AirportsLoading();
+}
+
+class AirportsSuccess extends AirportsState {
+  final List<AirportModel> airports;
+
+  const AirportsSuccess({required this.airports});
+}
+
+class AirportsError extends AirportsState {
+  final String error;
+
+  const AirportsError({required this.error});
 }
--- app/mobile/app/lib/cubit/predict_delay_cubit.dart
+++ app/mobile/app/lib/cubit/predict_delay_cubit.dart
@@ -3,13 +3,46 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../models/predict_delay_response_model.dart';
 import '../repository/flights_repository.dart';
 
-class PredictDelayCubit extends Cubit<PredictDelayResponseModel?> {
+class PredictDelayError extends PredictDelayState {
+  final String error;
+
+  const PredictDelayError({required this.error});
+}
+
+/// Cubit for predicting flight delays
+class PredictDelayCubit extends Cubit<PredictDelayState> {
   final FlightsRepository _repository;
 
-  PredictDelayCubit(this._repository) : super(null);
+  PredictDelayCubit(this._repository) : super(const PredictDelayInitial());
 
-  void predictDelay(String dayOfWeek, String airportId) {
-    final response = _repository.predictDelay(dayOfWeek, airportId);
-    emit(response);
+  Future<void> predictDelay(String dayOfWeek, String airportId) async {
+    emit(const PredictDelayLoading());
+
+    final result = await _repository.predictDelay(dayOfWeek, airportId);
+
+    result.fold(
+      (failure) => emit(
+          PredictDelayError(error: failure.message ?? 'An error occurred')),
+      (response) => emit(PredictDelaySuccess(response: response)),
+    );
   }
+}
+
+/// States for PredictDelayCubit
+sealed class PredictDelayState {
+  const PredictDelayState();
+}
+
+class PredictDelayInitial extends PredictDelayState {
+  const PredictDelayInitial();
+}
+
+class PredictDelayLoading extends PredictDelayState {
+  const PredictDelayLoading();
+}
+
+class PredictDelaySuccess extends PredictDelayState {
+  final PredictDelayResponseModel response;
+
+  const PredictDelaySuccess({required this.response});
 }
