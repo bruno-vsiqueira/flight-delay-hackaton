515207e49012e956cb6da13cb5e8995700ec5c26
Diogo Soares <diogo.soares@saltpay.co>
2025-04-30 13:55:42 +0100

    added skeleton for the backend project

---
+++ app/server/server/src/main/java/com/teya/hackaton/flight_delay/FlightDelayApplication.java
@@ -0,0 +1,13 @@
+package com.teya.hackaton.flight_delay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightDelayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightDelayApplication.class, args);
+	}
+
+}
---
+++ app/server/server/src/test/java/com/teya/hackaton/flight_delay/FlightDelayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teya.hackaton.flight_delay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightDelayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
